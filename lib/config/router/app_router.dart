ebb8df1e63cb80583de2021e538e637d2daeb66c
FMorinaDev <francomorina@outlook.com>
2024-01-25 16:04:44 -0300

    ShellRoute manteniendo el botton navigation bar

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -6,7 +6,37 @@ import 'package:go_router/go_router.dart';
 final appRouer = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
+    ShellRoute(
+      builder: (context, state, child){
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state){
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: PeliculaScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return PeliculaScreen( movieId: movieId,);
+              },
+            )
+          ]
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state){
+            return const FavoritosView();
+          }
+        ),
+      ]
+    )
+    //Rutas padre/hijas
+    /* GoRoute(
       path: '/',
       name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(childView: HomeView(),),
@@ -20,7 +50,7 @@ final appRouer = GoRouter(
           },
         )
       ]
-    ),
+    ), */
     
   ]
 );
