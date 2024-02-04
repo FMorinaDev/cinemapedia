d56e7ba7cf3da378c491081eb2122d625458a9e7
FMorinaDev <francomorina@outlook.com>
2024-02-04 15:31:34 -0300

    cartel de que no hay favoitos

--- lib/presentation/views/home_views/favoritos_view.dart
+++ lib/presentation/views/home_views/favoritos_view.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritosView extends ConsumerStatefulWidget {
   const FavoritosView({super.key});
@@ -35,6 +36,23 @@ class FavoritosViewState extends ConsumerState<FavoritosView> {
   @override
   Widget build(BuildContext context) {
     final peliculasFavoritas = ref.watch(favoritasPelicuasProvider).values.toList();
+
+    if (peliculasFavoritas.isEmpty) {
+      final colores = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60,color: colores.primary,),
+            Text('Ohhh no!!', style: TextStyle(fontSize: 30, color: colores.primary)),
+            const Text('No tienes peliculas favoritas', style: TextStyle(fontSize: 20, color: Colors.black45),),
+            const SizedBox(height: 20,),
+            FilledButton.tonal(onPressed: () => context.go('/'), child: const Text('Empieza a buscar'))
+          ],
+        ),
+      );
+    }
     return Scaffold(
       body: PeliculasMasonry(
         loadNextPage: loadNextPage,
